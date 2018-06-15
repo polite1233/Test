3d61f30bdf1f89748366d4f7c1c904fc91af1d9d
wang wanqing <wwq@wangdeMacBook-Pro.local>
2018-06-15 20:08:15 +0800

    upload

---
+++ src/main/java/com/github/wwq/first/FirstApplication.java
@@ -0,0 +1,23 @@
+package com.github.wwq.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class FirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApplication.class, args);
+    }
+}
+
+@RestController
+class FirstController {
+
+    @RequestMapping("/get")
+    public String get() {
+        return "1";
+    }
+}
